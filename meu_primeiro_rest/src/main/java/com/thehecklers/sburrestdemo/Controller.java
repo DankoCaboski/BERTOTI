c9250cb58b7b6a10d057527fe2ce142326ce837d
Willian Danko Caboski <caboskidanko@gmail.com>
2023-08-21 22:42:58 -0300

    Meu primeiro rest

---
+++ meu_primeiro_rest/src/main/java/com/thehecklers/sburrestdemo/Controller.java
@@ -0,0 +1,80 @@
+package com.thehecklers.sburrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootApplication
+public class Controller {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Controller.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/coffees")
+@CrossOrigin(origins = "*")
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoController() {
+		coffees.addAll(List.of(
+				new Coffee("Café Cereza"),
+				new Coffee("Café Ganador"),
+				new Coffee("Café Lareño"),
+				new Coffee("Café Três Pontas")
+		));
+	}
+
+	@GetMapping
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		for (Coffee c: coffees) {
+			if (c.getId().equals(id)) {
+				return Optional.of(c);
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		coffees.add(coffee);
+		return coffee;
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id,
+									 @RequestBody Coffee coffee) {
+		int coffeeIndex = -1;
+
+		for (Coffee c: coffees) {
+			if (c.getId().equals(id)) {
+				coffeeIndex = coffees.indexOf(c);
+				coffees.set(coffeeIndex, coffee);
+			}
+		}
+
+		return (coffeeIndex == -1) ?
+				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
+				new ResponseEntity<>(coffee, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteCoffee(@PathVariable String id) {
+		coffees.removeIf(c -> c.getId().equals(id));
+	}
+}
