d5e395654ac06c4c05e328f6789e1e98de054721
Willian Danko Caboski <caboskidanko@gmail.com>
2023-08-27 16:12:21 -0300

    Implementação das rotas basicas

--- rest_voos/src/main/java/com/Controller.java
+++ rest_voos/src/main/java/com/Controller.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
 public class Controller {
@@ -32,5 +31,32 @@ class Rotas {
 	@GetMapping
 	Iterable<Voo> getCoffees() {
 		return voos;
+	}
+
+	@PostMapping
+	ResponseEntity<String> createVoo() {
+		Voo novoVoo = new Voo();
+		voos.add(novoVoo);
+		return new ResponseEntity<>("Novo voo criado com sucesso", HttpStatus.CREATED);
+	}
+
+	@GetMapping("/{id}")
+	ResponseEntity<Voo> getVooById(@PathVariable int id) {
+		if (id >= 0 && id < voos.size()) {
+			Voo voo = voos.get(id);
+			return new ResponseEntity<>(voo, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	ResponseEntity<String> deleteVooById(@PathVariable int id) {
+		if (id >= 0 && id < voos.size()) {
+			voos.remove(id);
+			return new ResponseEntity<>("Voo removido com sucesso", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Voo não encontrado", HttpStatus.NOT_FOUND);
+		}
 	}
 }
